58a306f543ff74a714861b89393b43474551c4c4
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2024-10-29 10:52:08 -0400

    Make account number an int
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- Build a Watson AI Chatbot from Db2/server/account.sql
+++ Build a Watson AI Chatbot from Db2/server/account.sql
@@ -8,17 +8,17 @@ SET CURRENT SCHEMA CONNECTME;
 CREATE OR REPLACE TABLE ACCOUNT (
     EMAIL_ADDRESS VARCHAR(100) NOT NULL,
     NAME VARCHAR(100) NOT NULL,
-    ACCOUNT_NUMBER CHAR(4) NOT NULL,
+    ACCOUNT_NUMBER INT NOT NULL,
     PRIMARY KEY(EMAIL_ADDRESS)
 )
 ON REPLACE DELETE ROWS;
 
 -- Insert Data
 INSERT INTO ACCOUNT (EMAIL_ADDRESS, NAME, ACCOUNT_NUMBER) VALUES
-('john.doe@example.com', 'John Doe', 'A123'),
-('jane.smith@example.com', 'Jane Smith', 'B456'),
-('alice.jones@example.com', 'Alice Jones', 'C789'),
-('bob.brown@example.com', 'Bob Brown', 'D012');
+('john.doe@example.com', 'John Doe', '1111'),
+('jane.smith@example.com', 'Jane Smith', '2222'),
+('alice.jones@example.com', 'Alice Jones', '3333'),
+('bob.brown@example.com', 'Bob Brown', '4444');
 
 -- Query Table
 SELECT * FROM CONNECTME.ACCOUNT;
